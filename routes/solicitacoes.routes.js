a1124b962d544904acc6f39fc0df0c7362a649c3
Lucca Freitas Rodrigues <145407726+freitaslucca@users.noreply.github.com>
2025-04-03 16:08:55 -0300

    feat: criada rota para notificações e exportação de csv

--- routes/solicitacoes.routes.js
+++ routes/solicitacoes.routes.js
@@ -1,4 +1,5 @@
-const { criarSolicitacaoHandler, atualizarStatusHandler } = require('../controllers/solicitacoes.controller');
+const { criarSolicitacaoHandler, atualizarStatusHandler,painelCompradoraHandler,exportarSolicitacoesHandler } = require('../controllers/solicitacoes.controller');
+const{ getNotificacoesHandler } = require('../controllers/notificacoes.controller')
 const authenticate = require('../middlewares/auth');
 
 async function solicitacoesRoutes(fastify, options) {
@@ -8,6 +9,13 @@ async function solicitacoesRoutes(fastify, options) {
     preHandler: [authenticate],
     handler: atualizarStatusHandler
   });
+    // Rota para o painel da compradora
+    fastify.get('/painel/compradora', { preHandler: [authenticate] }, painelCompradoraHandler);
+
+    // Nova rota para pegar notificações
+    fastify.get('/notificacoes', { preHandler: [authenticate] }, getNotificacoesHandler);
+
+    fastify.get('/solicitacoes/exportar', { preHandler: [authenticate] }, exportarSolicitacoesHandler);
 }
 
 module.exports = solicitacoesRoutes;
